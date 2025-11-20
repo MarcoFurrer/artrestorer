f9649db98ff94266192ac1af936701755959483c
Nils Schell <nils.schell@stud.hslu.ch>
2025-11-20 13:25:50 +0100

    add service account JSON and update bucket name

--- bucket_access.py
+++ bucket_access.py
@@ -1,6 +1,6 @@
 from google.cloud import storage
 
 # Pfad zur JSON-Datei, die du ihnen geschickt hast
-client = storage.Client.from_service_account_json('path/to/keyfile.json')
-bucket = client.get_bucket('dein-bucket-name')
+client = storage.Client.from_service_account_json('aicomp-477516-a9f4b6a5785d.json')
+bucket = client.get_bucket('artrestorer')
 # Jetzt haben sie Zugriff
